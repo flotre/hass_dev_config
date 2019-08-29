6bb18d881cc67171f4a6d1d78d0ef2202d91ff96
Florent AUMAITRE <florent.aumaitre@gmail.com>
2019-08-29 23:42:52 +0200

    use properties to map schedule value

--- www/new-schedule.js
+++ www/new-schedule.js
@@ -4,8 +4,13 @@ import {
     css
   } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 import { repeat } from "https://unpkg.com/lit-html/directives/repeat.js?module";
-  
 
+
+const MODES_COLOR = {
+    eco: 'lightblue',
+    comfort: 'red',
+    away: 'green'
+};
 
   
 class ScheduleCard extends LitElement {
@@ -15,13 +20,18 @@ class ScheduleCard extends LitElement {
     }
     static get properties() {
         return {
-          myProp: {type: Number},
+          schedule: {type: Array},
         };
     }
 
     constructor() {
         super();
-        this.myProp = 1;
+        this._hours = [];
+        this._weekdays = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+        for(var i=0; i<24; i+=0.5) {
+            this._hours.push(i);
+        }
+        this.schedule = Array(this._weekdays.length).fill(0).map(x => Array(this._hours.length).fill("eco"));
     }
   
   
@@ -32,15 +42,7 @@ class ScheduleCard extends LitElement {
     setConfig(config) {
       this._config = config;
       this._select_start = null;
-      this._hours = [];
-      this._weekdays = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
-      /*for(var i=0; i<=24*60; i+=30) {
-          this._hours.push(this._pad(Math.floor(i/60))+":"+this._pad(((i/30)%2)*30));
-      }
-      console.log(this._hours);*/
-      for(var i=0; i<24; i+=0.5) {
-        this._hours.push(i);
-        }
+      
     }
 
     _pad(d) {
@@ -69,7 +71,7 @@ class ScheduleCard extends LitElement {
                     html`<div class="entete">${day}</div>
                     ${repeat(this._hours, (item) => item,
                         (hour, hindex) =>
-                        html`<div id="${dindex}-${hindex}" class="item" style="background-color: lightblue;"
+                        html`<div id="${dindex}-${hindex}" class="item" style="background-color: ${MODES_COLOR[this.schedule[dindex][hindex]]};"
                                 @click="${this._onclick}"
                                 @pointerdown="${this._onpointerdown}"
                                 @pointerup="${this._onpointerup}"
@@ -81,13 +83,14 @@ class ScheduleCard extends LitElement {
                 )}
                 
             </div>
-            <paper-dropdown-menu label="Mode">
-                <paper-listbox slot="dropdown-content" selected="${this.myProp}" @selected-changed="${this._modeSelected}">
-                    <paper-item>eco</paper-item>
-                    <paper-item>comfort</paper-item>
-                    <paper-item>away</paper-item>
-                </paper-listbox>
-            </paper-dropdown-menu>
+            <div>
+                <label id="label2">Mode:</label>
+                <paper-radio-group selected="${Object.keys(MODES_COLOR)[0]}" aria-labelledby="label2" @selected-changed="${this._modeSelected}">
+                    ${Object.keys(MODES_COLOR).map(mode => html`
+                    <paper-radio-button name="${mode}">${mode}</paper-radio-button>
+                    `)}
+                </paper-radio-group>
+            </div>
         </ha-card>
       `;
     }
@@ -109,31 +112,41 @@ class ScheduleCard extends LitElement {
     }
 
     _onclick(ev) {
-        console.log("on click", ev, this.myProp);
         if(ev) {
-            ev.target.style.background = "#ff77ee";
+            //ev.target.style.background = "#ff77ee";
+            console.log("on click", ev.target);
+            const id = ev.target.id;
+            const iday = parseInt(id.split('-')[0], 10);
+            const ihours = parseInt(id.split('-')[1], 10);
+            console.log("on click", id, iday, ihours, this.schedule);
+            // copy array
+            const newschedule = [...this.schedule];
+            newschedule[iday][ihours] = this.mode;
+            console.log(newschedule);
+            this.schedule = newschedule;
         }
     }
 
     _onpointerdown(ev) {
-        console.log("on pointerdown", ev);
+        //console.log("on pointerdown", ev);
         this._select_start = ev.target;
     }
   
     _onpointerup(ev) {
-        console.log("on pointerup", ev);
+        //console.log("on pointerup", ev);
         this._select_start = null;
     }
     
     _onpointerenter(ev) {
         
         if( this._select_start ) {
-            ev.target.style.background = "#ff77ee";
+            //ev.target.style.background = "#ff77ee";
         }
     }
 
     _modeSelected(ev) {
-        console.log("on _modeSelected", ev);
+        this.mode = ev.target.selected;
+        console.log("mode", ev.target, this.mode);
     }
 }
   
